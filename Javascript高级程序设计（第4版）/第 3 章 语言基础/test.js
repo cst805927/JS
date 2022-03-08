b02745cf7886f7fe43ef4767073d70628acb0cbe
cst805927 <cst698@163.com>
2022-03-08 17:02:19 +0800

    3.4.7（2）

--- Javascript高级程序设计（第4版）/第 3 章 语言基础/test.js
+++ Javascript高级程序设计（第4版）/第 3 章 语言基础/test.js
@@ -1,17 +1,3 @@
-function printRaw(strings) {
-    console.log('Actual characters:');
-    for (const string of strings) {
-        console.log(string);
-    }
-    console.log('Escaped characters:');
-    for (const rawString of strings.raw) {
-        console.log(rawString);
-    }
-}
-printRaw `\u00A9${'and'}\n`;
-// Actual characters:
-// ©
-// (换行符)
-// Escaped characters:
-// \u00A9
-// \n
+let fooGlobalSymbol = Symbol.for('foo'); // 创建新符号
+let otherFooGlobalSymbol = Symbol.for('foo'); // 重用已有符号
+console.log(fooGlobalSymbol === otherFooGlobalSymbol); // true
