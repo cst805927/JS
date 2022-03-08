9311ae7d34ba579394e09d8beeb1e7df7129a861
cst805927 <cst698@163.com>
2022-03-08 21:34:39 +0800

    3.4.7 (6)

--- Javascript高级程序设计（第4版）/第 3 章 语言基础/test.js
+++ Javascript高级程序设计（第4版）/第 3 章 语言基础/test.js
@@ -1,13 +1,11 @@
-let o = {
-    [Symbol('foo')]: 'foo val',
-    [Symbol('bar')]: 'bar val'
-};
-
-// {Symbol(foo): "foo val", Symbol(bar): "bar val"}
-console.log(o);
-
-let barSymbol = Object.getOwnPropertySymbols(o)
-    .find((symbol) => symbol.toString().match(/bar/));
-
-// Symbol(bar)
-console.log(barSymbol);
+class Bar {}
+class Baz extends Bar {
+    static[Symbol.hasInstance]() {
+        return false;
+    }
+}
+let b = new Baz();
+console.log(Bar[Symbol.hasInstance](b)); // true
+console.log(b instanceof Bar); // true
+console.log(Baz[Symbol.hasInstance](b)); // false
+console.log(b instanceof Baz); // false
