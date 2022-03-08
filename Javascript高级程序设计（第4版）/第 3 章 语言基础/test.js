b02c401924032aac6e95f878df1c3b460d2eac38
cst805927 <cst698@163.com>
2022-03-08 20:35:41 +0800

    3.4.7(4)

--- Javascript高级程序设计（第4版）/第 3 章 语言基础/test.js
+++ Javascript高级程序设计（第4版）/第 3 章 语言基础/test.js
@@ -1,3 +1,13 @@
-let fooGlobalSymbol = Symbol.for('foo'); // 创建新符号
-let otherFooGlobalSymbol = Symbol.for('foo'); // 重用已有符号
-console.log(fooGlobalSymbol === otherFooGlobalSymbol); // true
+let o = {
+    [Symbol('foo')]: 'foo val',
+    [Symbol('bar')]: 'bar val'
+};
+
+// {Symbol(foo): "foo val", Symbol(bar): "bar val"}
+console.log(o);
+
+let barSymbol = Object.getOwnPropertySymbols(o)
+    .find((symbol) => symbol.toString().match(/bar/));
+
+// Symbol(bar)
+console.log(barSymbol);
