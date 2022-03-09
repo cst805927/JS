8b733b2fac097e9d5cb8ab656d88f07e2dcd506d
cst805927 <cst698@163.com>
2022-03-09 16:54:32 +0800

    3.5.3

--- Javascript高级程序设计（第4版）/第 3 章 语言基础/test.js
+++ Javascript高级程序设计（第4版）/第 3 章 语言基础/test.js
@@ -1,16 +1,1 @@
-let s1 = "01";
-let s2 = "1.1";
-let s3 = "z";
-let b = false;
-let f = 1.1;
-let o = {
-    valueOf () {
-        return -1;
-    }
-}
-s1 = -s1; // -1
-s2 = -s2; // -1.1
-s3 = -s3; // NaN
-b = -b; // 0
-f = -f; // -1.1
-o = -o; // 1
+let myObject = preferredObject || backupObject;
