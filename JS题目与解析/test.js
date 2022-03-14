bb4034c890f8237f9936e8146b9eada7a1768473
cst805927 <cst698@163.com>
2022-03-14 20:53:16 +0800

    真题 3.1（224-233）

--- JS题目与解析/test.js
+++ JS题目与解析/test.js
@@ -1,4 +1,13 @@
-var total = sum
-(x + y);
-//相当于
-var total = sum(x + y);
+function digit(i) {
+    if (i = 2) {
+        i++;
+    } else if (i == 3) {
+        i += 2;
+    } else if (i == 4) {
+        ++i;
+    }
+    return 1;
+}
+for (var i = 1; i < 5; iH + ) {
+    console.log(digit(i));
+}
