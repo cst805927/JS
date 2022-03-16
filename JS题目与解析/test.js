9aa8a93b208a833c1bd26c2b0c7973ff32f7dd89
cst805927 <cst698@163.com>
2022-03-16 10:59:00 +0800

    5.3.1

--- JS题目与解析/test.js
+++ JS题目与解析/test.js
@@ -1,6 +1,14 @@
-let text = "this has been a short summer";
-let pattern = /(..)or(.)/g;
-if (pattern.test(text)) {
-    console.log(RegExp.$1); // sh 
-    console.log(RegExp.$2); // t
-}
+let pattern1 = /\[bc\]at/i;
+console.log(pattern1.global); // false 
+console.log(pattern1.ignoreCase); // true
+console.log(pattern1.multiline); // false 
+console.log(pattern1.lastIndex); // 0 
+console.log(pattern1.source); // "\[bc\]at" 
+console.log(pattern1.flags); // "i"
+let pattern2 = new RegExp("\\[bc\\]at", "i");
+console.log(pattern2.global); // false 
+console.log(pattern2.ignoreCase); // true 
+console.log(pattern2.multiline); // false 
+console.log(pattern2.lastIndex); // 0 
+console.log(pattern2.source); // "\[bc\]at"
+console.log(pattern2.flags); // "i"
