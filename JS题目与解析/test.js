96b2abbf84fc464a6261a86c5ae057bd1b4e932b
cst805927 <cst698@163.com>
2022-03-20 15:53:59 +0800

    真题 3.2

--- JS题目与解析/test.js
+++ JS题目与解析/test.js
@@ -1,14 +1,8 @@
-let pattern1 = /\[bc\]at/i;
-console.log(pattern1.global); // false 
-console.log(pattern1.ignoreCase); // true
-console.log(pattern1.multiline); // false 
-console.log(pattern1.lastIndex); // 0 
-console.log(pattern1.source); // "\[bc\]at" 
-console.log(pattern1.flags); // "i"
-let pattern2 = new RegExp("\\[bc\\]at", "i");
-console.log(pattern2.global); // false 
-console.log(pattern2.ignoreCase); // true 
-console.log(pattern2.multiline); // false 
-console.log(pattern2.lastIndex); // 0 
-console.log(pattern2.source); // "\[bc\]at"
-console.log(pattern2.flags); // "i"
+function reverse(str) {
+    let result = "",
+        begin = str.length - 1;
+    for (let i = begin; i >= 0; i--) {
+        result += str[i];
+    }
+    return result;
+}
