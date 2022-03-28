2dfae76b22638cd69fafa1b79a71043119aeb9e6
cst805927 <cst698@163.com>
2022-03-28 22:08:58 +0800

    11.2.3

--- Javascript高级程序设计（第4版）/第 11 章 期约与异步函数/test.js
+++ Javascript高级程序设计（第4版）/第 11 章 期约与异步函数/test.js
@@ -1,12 +1,28 @@
+console.log('begin synchronous execution');
 try {
-    throw new Error('foo');
+    throw Error('foo');
 } catch (e) {
-    console.log(e); // Error: foo
+    console.log('caught error', e);
 }
+console.log('continue synchronous execution');
 
-try {
-    Promise.reject(new Error('bar'));
-} catch (e) {
-    console.log(e);
-}
-// Uncaught (in promise) Error: bar
+// begin synchronous execution
+// caught error Error: foo
+// continue synchronous execution 
+
+new Promise((resolve, reject) => {
+
+    console.log('begin asynchronous execution');
+
+    reject(Error('bar'));
+}).catch((e) => {
+
+    console.log('caught error', e);
+}).then(() => {
+
+    console.log('continue asynchronous execution');
+});
+
+// begin asynchronous execution
+// caught error Error: bar
+// continue asynchronous execution
