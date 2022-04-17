bba03a71625366579733cf1c0c9282af853901a8
cst805927 <cst698@163.com>
2022-04-17 10:59:58 +0800

    17.4.1 04

--- Javascript高级程序设计（第4版）/第 17 章 事件/test.js
+++ Javascript高级程序设计（第4版）/第 17 章 事件/test.js
@@ -1,10 +1,7 @@
-let btn = document.getElementById("myBtn");
-let handler = function () {
-	console.log(this.id);
-};
-
-btn.addEventListener("click", handler, false);
-
-// 其他代码
-
-btn.removeEventListener("click", handler, false); // 有效果！
+window.addEventListener("scroll", (event) => {
+	if (document.compatMode == "CSS1Compat") {
+		console.log(document.documentElement.scrollTop);
+	} else {
+		console.log(document.body.scrollTop);
+	}
+});
