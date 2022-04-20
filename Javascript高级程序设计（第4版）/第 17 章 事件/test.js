12cc574177f7f6cf6b4284289b5e95bcd77f616e
cst805927 <cst698@163.com>
2022-04-20 10:23:15 +0800

    17.4.9 01 xmind

--- Javascript高级程序设计（第4版）/第 17 章 事件/test.js
+++ Javascript高级程序设计（第4版）/第 17 章 事件/test.js
@@ -1,10 +1,15 @@
-let textbox = document.getElementById("myText");
-textbox.addEventListener("compositionstart", (event) => {
-	console.log(event.data);
-});
-textbox.addEventListener("compositionupdate", (event) => {
-	console.log(event.data);
-});
-textbox.addEventListener("compositionend", (event) => {
-	console.log(event.data);
+let list = document.getElementById("myLinks");
+list.addEventListener("click", (event) => {
+	let target = event.target;
+	switch (target.id) {
+		case "doSomething":
+			document.title = "I changed the document's title";
+			break;
+		case "goSomewhere":
+			location.href = "http:// www.wrox.com";
+			break;
+		case "sayHi":
+			console.log("hi");
+			break;
+	}
 });
