35b6a60053f215c343efcaaafff14bf426f1b048
cst805927 <cst698@163.com>
2022-04-21 10:46:26 +0800

    19.2.1 02 md

---
+++ Javascript高级程序设计（第4版）/第 19 章 表单脚本/test.js
@@ -0,0 +1,10 @@
+function getSelectedText(textbox) {
+	if (typeof textbox.selectionStart == "number") {
+		return textbox.value.substring(
+			textbox.selectionStart,
+			textbox.selectionEnd
+		);
+	} else if (document.selection) {
+		return document.selection.createRange().text;
+	}
+}
