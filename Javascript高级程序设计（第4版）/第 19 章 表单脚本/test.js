e4f3554c36f5eac58b03162bc1b3e79c61246287
cst805927 <cst698@163.com>
2022-04-24 10:39:51 +0800

    第19章md,xmind

--- Javascript高级程序设计（第4版）/第 19 章 表单脚本/test.js
+++ Javascript高级程序设计（第4版）/第 19 章 表单脚本/test.js
@@ -1,10 +1,12 @@
-function getSelectedText(textbox) {
-	if (typeof textbox.selectionStart == "number") {
-		return textbox.value.substring(
-			textbox.selectionStart,
-			textbox.selectionEnd
-		);
-	} else if (document.selection) {
-		return document.selection.createRange().text;
-	}
-}
+let selection = frames["richedit"].getSelection();
+
+// 取得选中的文本 
+let selectedText = selection.toString();
+
+// 取得表示选区的范围 
+let range = selection.getRangeAt(0);
+
+// 高亮选中的文本 
+let span = frames["richedit"].document.createElement("span");
+span.style.backgroundColor = "yellow";
+range.surroundContents(span)
