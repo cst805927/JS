03cddad761e4b774605be3af5096a784ed87d9ba
cst805927 <cst698@163.com>
2022-04-29 16:28:42 +0800

    第24章 md

--- Javascript高级程序设计（第4版）/第 24 章 网络请求与远程资源/test.js
+++ Javascript高级程序设计（第4版）/第 24 章 网络请求与远程资源/test.js
@@ -1,8 +1,3 @@
-function handleResponse(response) {
- console.log(`
- You're at IP address ${response.ip}, which is in
- ${response.city}, ${response.region_name}`);
-}
-let script = document.createElement("script");
-script.src = "http://freegeoip.net/json/?callback=handleResponse";
-document.body.insertBefore(script, document.body.firstChild);
+socket.onclose = function (event) {
+    console.log(`as clean? ${event.wasClean} Code=${event.code} Reason=${ event.reason}`);
+};
