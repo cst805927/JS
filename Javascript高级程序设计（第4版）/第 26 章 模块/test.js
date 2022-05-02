f3bf959f87c17b70d72e412915e6f72970dfd6a7
cst805927 <cst698@163.com>
2022-05-02 22:08:17 +0800

    第26章 md

--- Javascript高级程序设计（第4版）/第 26 章 模块/test.js
+++ Javascript高级程序设计（第4版）/第 26 章 模块/test.js
@@ -1,4 +1,4 @@
-console.log('moduleA');
-if (loadCondition) {
-    require('./moduleA');
-}
+// 不支持模块的浏览器不会执行这里的代码
+<script type="module" src="module.js"></script> 
+// 不支持模块的浏览器会执行这里的代码
+ <script src="script.js"></script>
