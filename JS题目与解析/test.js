81ad5bd3da9de0cdfa2b51840a5e35e80f7935e0
cst805927 <cst698@163.com>
2022-05-04 08:37:31 +0800

    真题 3.10 md

--- JS题目与解析/test.js
+++ JS题目与解析/test.js
@@ -1,16 +1,4 @@
-let promise = new Promise(function (fulfilled) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("post", "server.php");
-    xhr.onload = function () {
-        fulfilled.call(this);
-    };
-    xhr.send(null);
-});
-
-promise.then(function () {
-    console.log("nest1");
-}).then(function () {
-    console.log("nest2");
-}).then(function () {
-    console.log("nest3");
-});
+$("#name").val(); // 第一种
+$("#name").find("option:selected").val(); // 第二种
+let index = $("#name").get(0).selectedIndex; // 第三种
+$("#name").find("option:eq(" + index + ")").val();
